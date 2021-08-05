d1e6bcb2c8854859ccdcbc3d801f5f66734840c9
GeethanjaliAnbalagan <84552111+GeethanjaliAnbalagan@users.noreply.github.com>
2021-08-05 14:36:22 +0530

    Add files via upload

---
+++ demo1/conditionalif.java
@@ -0,0 +1,15 @@
+package javaf;
+
+public class conditionalif {
+	public static void main(String[] args)
+	{
+	    int time = 2;
+	    if (time < 18) {
+	      System.out.println("Good day.");
+	    System.out.println("Good day.");}
+	     else {
+	      System.out.println("Good evening.");
+	    }  
+	  }
+
+}
